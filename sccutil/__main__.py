796258bf888aac5a27b7a96f7836ebf9bbde6b29
jgould <jgould@broadinstitute.org>
2019-09-05 07:30:50 -0400

    Added parse monitoring log
    Former-commit-id: cf3fe3a61ff81338abe526786ce41d481a251c34

--- sccutil/__main__.py
+++ sccutil/__main__.py
@@ -6,7 +6,7 @@ import argparse
 
 
 def main():
-    command_list = [fc_add_method, fc_inputs, fc_remove_method, fc_run, sample_sheet, fc_upload]
+    command_list = [fc_add_method, fc_inputs, fc_remove_method, fc_run, sample_sheet, fc_upload, parse_monitoring_log]
     parser = argparse.ArgumentParser(description='Run a sccutil command')
     command_list_strings = list(map(lambda x: x.__name__[len('sccutil.commands.'):], command_list))
     parser.add_argument('command', help='The command', choices=command_list_strings)
--- sccutil/commands/parse_monitoring_log.py
+++ sccutil/commands/parse_monitoring_log.py
@@ -2,23 +2,48 @@ import argparse
 
 
 def main(argsv):
-    parser = argparse.ArgumentParser(description='Output maximum CPU, memory, and disk from monitoring.log file')
-    parser.add_argument(dest='path', help='Path to monitoring.log file.', required=True)
+    parser = argparse.ArgumentParser(description='Output maximum CPU, memory, and disk from monitoring log file')
+    parser.add_argument(dest='path', help='Path to monitoring log file.')
+    parser.add_argument('--plot', dest='plot', action='store', required=False,
+        help='Optional filename to create a plot of utilization vs. time')
     args = parser.parse_args(argsv)
+    do_plot = args.plot is not None
+
+    path = args.path
+    if do_plot:
+        from dateutil.parser import parse
+        import matplotlib.pyplot as plt
     max_memory_percent = 0
     max_cpu_percent = 0
     max_disk_percent = 0
-    path = args.path
+
+    times = []
+    cpu_times = []
+    memory_times = []
+    disk_times = []
+    cpu = None
+    memory = None
+    disk = None
     with open(path) as f:
         for line in f:
             line = line.strip()
-
+            if line.startswith('[') and line.endswith(']'):
+                times.append(parse(line[1:len(line) - 1]))
             if line.startswith('* CPU usage:'):
-                max_cpu_percent = max(max_cpu_percent, float(line[line.index(':') + 1:len(line) - 1]))
+                value = float(line[line.index(':') + 1:len(line) - 1])
+                if do_plot:
+                    cpu_times.append(value)
+                max_cpu_percent = max(max_cpu_percent, value)
             elif line.startswith('* Memory usage:'):
-                max_memory_percent = max(max_memory_percent, float(line[line.index(':') + 1:len(line) - 1]))
+                value = float(line[line.index(':') + 1:len(line) - 1])
+                if do_plot:
+                    memory_times.append(value)
+                max_memory_percent = max(max_memory_percent, value)
             elif line.startswith('* Disk usage:'):
-                max_disk_percent = max(max_disk_percent, float(line[line.index(':') + 1:len(line) - 1]))
+                value = float(line[line.index(':') + 1:len(line) - 1])
+                if do_plot:
+                    disk_times.append(value)
+                max_disk_percent = max(max_disk_percent, value)
             elif line.startswith('#CPU'):
                 cpu = int(line[line.index(':') + 1:len(line)])
             elif line.startswith('Total Memory:'):
@@ -26,6 +51,23 @@ def main(argsv):
             elif line.startswith('Total Disk space:'):
                 disk = float(line[line.index(':') + 1:len(line) - 1])
 
+    if do_plot:
+        plt.subplot(311)
+        plt.plot_date(times, cpu_times, '-')
+        plt.xlabel('Time')
+        plt.ylabel('% CPU')
+
+        plt.subplot(312)
+        plt.plot_date(times, memory_times, '-')
+        plt.xlabel('Time')
+        plt.ylabel('% Memory')
+
+        plt.subplot(313)
+        plt.plot_date(times, disk_times, '-')
+        plt.xlabel('Time')
+        plt.ylabel('% Disk')
+
+        plt.savefig(args.plot)
     print('Max cpu %: {}'.format(max_cpu_percent))
     print('Max memory %: {}'.format(max_memory_percent))
     print('Max disk %: {}'.format(max_disk_percent))
