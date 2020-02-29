44c7060a98b8158f136c583e9dcbd2cf5b35d51f
Bo Li <bli28@mgh.harvard.edu>
2020-02-29 15:31:01 -0500

    Changed get_latest_method to get_method with method_version option, if method_version is None, fetch the latest

--- alto/commands/__init__.py
+++ alto/commands/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from .fc_add_method import *
-from .fc_download_method import *
-from .fc_inputs import *
-from .fc_remove_method import *
-from .fc_run import *
-from .fc_upload import *
+from .add_method import *
+from .download_method import *
+from .remove_method import *
+from .inputs import *
+from .run import *
+from .upload import *
 from .parse_monitoring_log import *
 from .sample_sheet import *
