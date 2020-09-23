63e53f30ef17f0460db496b58e28b7ff307acded
jgould <jgould@broadinstitute.org>
2020-09-23 17:20:47 -0400

    Added storage cost estimate export
    Former-commit-id: 2f51ec37cc7a4de726e9579aa67a5afafddbe00d

--- alto/commands/__init__.py
+++ alto/commands/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from .add_method import *
-from .download_method import *
-from .remove_method import *
 from .create_input_stub import *
-from .run import *
-from .upload import *
+from .download_method import *
 from .parse_monitoring_log import *
+from .remove_method import *
+from .run import *
 from .sample_sheet import *
+from .upload import *
+from .workspace_storage_estimate import *
