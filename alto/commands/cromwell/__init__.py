916756d9138c5116ee36e54d810a5bccbe2066fb
Yiming Yang <yang.yihming@gmail.com>
2021-10-25 01:33:14 -0400

    Add get_logs and get_metadata subcommands of cromwell command
    Former-commit-id: cbdbaff8a2a7cdfbe0e2dfb3f47b2a7bd659677c

--- alto/commands/cromwell/__init__.py
+++ alto/commands/cromwell/__init__.py
@@ -1,11 +1,11 @@
 import sys, argparse
-from . import run, check_status, abort
+from . import run, check_status, abort, get_metadata, get_logs
 
 def main(args):
-    str2module = {'run': run, 'check_status': check_status, 'abort': abort}
+    str2module = {'run': run, 'check_status': check_status, 'abort': abort, 'get_metadata': get_metadata, 'get_logs': get_logs}
 
     parser = argparse.ArgumentParser(description='Run a terra sub-command.')
-    parser.add_argument('subcommand', help='The sub-command', choices=['run', 'check_status', 'abort'])
+    parser.add_argument('subcommand', help='The sub-command', choices=['run', 'check_status', 'abort', 'get_metadata', 'get_logs'])
     parser.add_argument('subcommand_args', help='The sub-command arguments', nargs=argparse.REMAINDER)
     my_args = parser.parse_args(args)
 
