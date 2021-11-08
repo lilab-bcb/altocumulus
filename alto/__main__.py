f9b4866434d5a9913bfbd74be342b2da47dc9053
Yiming Yang <yang.yihming@gmail.com>
2021-11-08 13:39:10 -0500

    reorg dependencies; add version checking command

--- alto/__main__.py
+++ alto/__main__.py
@@ -6,6 +6,10 @@ import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+try:
+    from importlib.metadata import version
+except ImportError:  # < Python 3.8: Use backport module
+    from importlib_metadata import version
 
 def main():
     str2module = {'terra': terra, 'upload': upload, 'parse_monitoring_log': parse_monitoring_log, 'cromwell': cromwell}
@@ -13,6 +17,7 @@ def main():
     parser = argparse.ArgumentParser(description='Run an altocumulus command.')
     parser.add_argument('command', help='The command', choices=['terra', 'upload', 'parse_monitoring_log', 'cromwell'])
     parser.add_argument('command_args', help='The command arguments', nargs=argparse.REMAINDER)
+    parser.add_argument('-v', '--version', action="version", version=version('altocumulus'))
     my_args = parser.parse_args()
 
     cmd = str2module[my_args.command]
