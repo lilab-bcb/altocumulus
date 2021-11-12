67451f2ba611cb0e8cada9ae8fcf390cbab33bb1
Yiming Yang <yang.yihming@gmail.com>
2021-11-12 18:54:46 -0500

    Create the folder for logs in any case

--- alto/commands/cromwell/get_logs.py
+++ alto/commands/cromwell/get_logs.py
@@ -1,4 +1,4 @@
-import argparse, json, requests
+import argparse, requests
 
 from alto.utils import run_command
 from subprocess import CalledProcessError
@@ -22,10 +22,14 @@ def get_remote_log_file(cloud_uri, job_id):
     except CalledProcessError:
         print(f"{cloud_uri} does not exist.")
 
+
 def get_logs_per_task(server, port, job_id, meta_dict):
     # For tasks with no subworkflow ID
     resp_top = requests.get(f"http://{server}:{port}/api/workflows/v1/{job_id}/logs")
     resp_top_dict = resp_top.json()
+
+    # Create log folder even if there will be no log file.
+    run_command(['mkdir', '-p', job_id], dry_run=False)
 
     processed_tasks = set()
     if 'calls' in resp_top_dict.keys():
@@ -35,7 +39,7 @@ def get_logs_per_task(server, port, job_id, meta_dict):
                 get_remote_log_file(log['stdout'], job_id)
             processed_tasks.add(task_name)
     else:
-        print("No call logs for this job.")
+        print("No call log for this job.")
 
     # For tasks with subworkflow ID
     if 'calls' in meta_dict.keys():
@@ -50,8 +54,7 @@ def get_logs_per_task(server, port, job_id, meta_dict):
                             get_remote_log_file(log['stderr'], job_id)
                             get_remote_log_file(log['stdout'], job_id)
     else:
-        print("No call logs for subworkflows of this job.")
-
+        print("No call log for subworkflows of this job.")
 
 
 def get_logs(server, port, job_id):
@@ -62,6 +65,7 @@ def get_logs(server, port, job_id):
         get_logs_per_task(server, port, job_id, meta_dict)
     else:
         print(meta_dict['message'])
+
 
 def main(argv):
     parser = argparse.ArgumentParser(
@@ -78,5 +82,4 @@ def main(argv):
     )
 
     args = parser.parse_args(argv)
-
     get_logs(args.server, args.port, args.job_id)
