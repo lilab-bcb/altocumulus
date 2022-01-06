432da7d1f197d40ec9f498c527d567a34996c120
Rimte Rocher <rimte.rocher@contractors.roche.com>
2022-01-06 07:16:06 -0500

    list jobs
    Former-commit-id: 4d2f059d23667c31f17cc3c429efb8f930f995e8

--- alto/commands/cromwell/list_jobs.py
+++ alto/commands/cromwell/list_jobs.py
@@ -1,90 +1,154 @@
-import argparse, getpass, json, requests
+import argparse
+import getpass
+import requests
 import pandas as pd
+from datetime import datetime
+from dateutil import parser
+import time
+
+
+def datetime_from_utc_to_local(utc_datetime: str) -> str:
+    if not utc_datetime:
+        return ""
+    now_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(
+        now_timestamp
+    )
+    return (parser.parse(utc_datetime) + offset).strftime("%d %b %Y, %H:%M:%S")
 
 
 class bcolors:
-    SUCCESS = '\033[92m'
-    FAILED = '\033[91m'
-    ENDC = '\033[0m'
+    SUCCESS = "\033[92m"
+    FAILED = "\033[91m"
+    ENDC = "\033[0m"
 
 
-def show_one_job(s, status):
-    if status == 'Succeeded':
+def show_one_job(s: str, status: str):
+    if status == "Succeeded":
         print(f"{bcolors.SUCCESS}{s}{bcolors.ENDC}")
-    elif status in ['Failed', 'Aborted']:
+    elif status in ["Failed", "Aborted"]:
         print(f"{bcolors.FAILED}{s}{bcolors.ENDC}")
     else:
         print(s)
 
 
-def show_jobs(df, top_level_only):
-    if top_level_only:
-        df = df.loc[df['parentWorkflowId'].isnull()].copy()
-
-    if 'creator' not in df.columns:
-        df['creator'] = ""
-
-    # Print headers
-    if top_level_only:
-        print("Job ID\tCreator\tWorkflow\tStatus\tSubmitted\tStart\tEnd")
-    else:
-        print("Job ID\tTask ID\tCreator\tWorkflow\tStatus\tSubmitted\tStart\tEnd")
+def show_jobs(df: pd.DataFrame):
+    if "creator" not in df.columns:
+        df["creator"] = ""
+    print(
+        "{:<38} {:<16} {:<24} {:<13} {:<28} {:<28} {:<28}".format(
+            "Job ID", "Creator", "Workflow", "Status", "Submitted", "Start", "End"
+        )
+    )
 
     for _, row in df.iterrows():
-        if top_level_only:
-            show_str = f"{row['id']}\t{row['creator']}\t{row['name']}\t{row['status']}\t{row['submission']}\t{row['start']}\t{row['end']}"
-        else:
-            show_str = f"{row['parentWorkflowId']}\t{row['id']}\t{row['creator']}\t{row['name']}\t{row['status']}\t{row['submission']}\t{row['start']}\t{row['end']}"
-        show_one_job(show_str, row['status'])
+        show_str = "{:<38} {:<16} {:<24} {:<13} {:<28} {:<28} {:<28}".format(
+            row["id"],
+            row["creator"],
+            row["name"],
+            row["status"],
+            row["submission"],
+            row["start"],
+            row["end"],
+        )
+        show_one_job(show_str, row["status"])
 
 
-def list_jobs(server, port, is_all, username, job_id, job_status):
-    top_level_only = True if job_id is None else False
-    query_data = dict()
-
+def list_jobs(
+    server: str, port: int, is_all: bool, username: str, job_id: str, job_status: str
+):
+    query_data = []
+    query_data.append({"additionalQueryResultFields": "labels"})
     if not is_all:
         if job_id is not None:
-            query_data['id'] = job_id
+            query_data.append({"id": job_id})
         else:
-            query_data['additionalQueryResultFields'] = ['labels']
-            query_data['label'] = [f"creator:{username}"]
+            query_data.append({"additionalQueryResultFields": "labels"})
+            query_data.append({"label": f"creator:{username}"})
 
-        if job_status is not None:
-            query_data['status'] = [job_status]
-
-    resp = requests.get(
+    if job_status is not None:
+        query_data.append({"status": job_status})
+    resp = requests.post(
         f"http://{server}:{port}/api/workflows/v1/query",
-        data=query_data,
+        json=query_data,
     )
     resp_dict = resp.json()
-
+    for res in resp_dict["results"]:
+        if "labels" in res:
+            res["creator"] = res["labels"].get("creator", "")
+            del res["labels"]
+        else:
+            res["creator"] = ""
+        res["submission"] = datetime_from_utc_to_local(res.get("submission", ""))
+        res["start"] = datetime_from_utc_to_local(res.get("start", ""))
+        res["end"] = datetime_from_utc_to_local(res.get("end", ""))
     if resp.status_code == 200:
-        df_jobs = pd.DataFrame.from_records(resp_dict['results'])
-        show_jobs(df_jobs, top_level_only)
+        df_jobs = pd.DataFrame.from_records(resp_dict["results"])
+        show_jobs(df_jobs)
     else:
-        print(resp_dict['message'])
+        print(resp_dict["message"])
+
 
 def main(argv):
-    parser = argparse.ArgumentParser(
-        description="List jobs submitted to the server."
+    parser = argparse.ArgumentParser(description="List jobs submitted to the server.")
+    parser.add_argument(
+        "-s",
+        "--server",
+        dest="server",
+        action="store",
+        required=True,
+        help="Server hostname or IP address.",
     )
-    parser.add_argument('-s', '--server', dest='server', action='store', required=True,
-        help="Server hostname or IP address."
+    parser.add_argument(
+        "-p",
+        "--port",
+        dest="port",
+        action="store",
+        default="8000",
+        help="Port number for Cromwell service. The default port is 8000.",
     )
-    parser.add_argument('-p', '--port', dest='port', action='store', default='8000',
-        help="Port number for Cromwell service. The default port is 8000."
+    parser.add_argument(
+        "-a",
+        "--all",
+        dest="is_all",
+        action="store_true",
+        default=False,
+        help="List all the jobs on the server.",
     )
-    parser.add_argument('-a', '--all', dest='is_all', action='store_true', default=False,
-        help="List all the jobs on the server."
+    parser.add_argument(
+        "-u",
+        "--user",
+        dest="user",
+        action="store",
+        default=getpass.getuser(),
+        help="List jobs submitted by this user.",
     )
-    parser.add_argument('-u', '--user', dest='user', action='store', default=getpass.getuser(),
-        help="List jobs submitted by this user."
+    parser.add_argument(
+        "--id",
+        dest="job_id",
+        action="store",
+        help="List all the subtasks of the specified job.",
     )
-    parser.add_argument('--id', dest='job_id', action='store',
-        help="List all the subtasks of the specified job."
+    parser.add_argument(
+        "--only-succeeded",
+        dest="only_succeeded",
+        action="store_true",
+        default=False,
+        help="Only show jobs succeeded.",
     )
-    parser.add_argument('--only-succeeded', dest='only_succeeded', action='store_true', default=False,
-        help="Only show jobs succeeded."
+    parser.add_argument(
+        "--only-running",
+        dest="only_running",
+        action="store_true",
+        default=False,
+        help="Only show jobs that are running.",
+    )
+    parser.add_argument(
+        "--only-failed",
+        dest="only_failed",
+        action="store_true",
+        default=False,
+        help="Only show jobs that have failed.",
     )
 
     args = parser.parse_args(argv)
@@ -92,6 +156,9 @@ def main(argv):
     job_status = None
     if args.only_succeeded:
         job_status = "Succeeded"
-
+    elif args.only_failed:
+        job_status = "Failed"
+    elif args.only_running:
+        job_status = "Running"
 
     list_jobs(args.server, args.port, args.is_all, args.user, args.job_id, job_status)
