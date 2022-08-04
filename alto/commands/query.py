0cb4e040641bb739ac22a44918011e34668f4543
Bo Li <li.bo@gene.com>
2022-08-04 10:01:24 -0700

    Added query command
    Former-commit-id: 4b9081da79daac0d33192ff4bf48667f9fbb7af7

---
+++ alto/commands/query.py
@@ -0,0 +1,32 @@
+import argparse
+from argparse import RawTextHelpFormatter
+import pandas as pd
+
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Query project metadata from a LIMS (Laboratory Information Management System) via RESTful APIs.\n\n\
+Given one project ID, this subcommand tries to fetch and display metadata from all libraries under the project. \
+This subcommand also provides an option to write the metadata into a CSV file.\n\n\
+Using this subcommand requires `lims_query` Python package installed. \
+Users who want to use this subcommand need to write their own lims_query package, which should contain one function:\n\
+    query_project_id(project_id: str) -> pd.DataFrame.', formatter_class=RawTextHelpFormatter)
+
+    parser.add_argument('pid', metavar='projectID', help='Project ID.')
+    parser.add_argument('-o', dest='csv_file', help='Write metadata information to a CSV file.')
+    
+    args = parser.parse_args(argv)
+
+    try:
+        from lims_query import query_project_id
+    except ImportError as e:
+        import sys
+        print(f"{e}\nPlease install lims_query package first.")
+        sys.exit(-1)
+
+    df = query_project_id(args.pid)
+
+    if args.csv_file != None:
+        df.to_csv(args.csv_file)
+
+    print(df.to_string())
