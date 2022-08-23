2706ddad4d3db92e5b59473a2a813788c6428463
Joshua Gould <joshua-gould@users.noreply.github.com>
2022-08-23 17:57:09 -0400

    Delete query command
    Former-commit-id: 3c88650da015efd74b5f65ae53c5c4f89af2f593

--- alto/__main__.py
+++ alto/__main__.py
@@ -1,9 +1,12 @@
 import sys
 import argparse
-from alto.commands import terra, upload, parse_monitoring_log, cromwell, query
 
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+from alto.commands import cromwell, parse_monitoring_log, terra, upload
+
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 try:
@@ -11,19 +14,29 @@ try:
 except ImportError:  # < Python 3.8: Use backport module
     from importlib_metadata import version
 
-def main():
-    str2module = {'terra': terra, 'upload': upload, 'parse_monitoring_log': parse_monitoring_log, 'cromwell': cromwell, 'query': query}
 
-    parser = argparse.ArgumentParser(description='Run an altocumulus command.')
-    parser.add_argument('command', help='The command', choices=['terra', 'upload', 'parse_monitoring_log', 'cromwell', 'query'])
-    parser.add_argument('command_args', help='The command arguments', nargs=argparse.REMAINDER)
-    parser.add_argument('-v', '--version', action="version", version=version('altocumulus'))
+def main():
+    str2module = {
+        "terra": terra,
+        "upload": upload,
+        "parse_monitoring_log": parse_monitoring_log,
+        "cromwell": cromwell,
+    }
+
+    parser = argparse.ArgumentParser(description="Run an altocumulus command.")
+    parser.add_argument(
+        "command",
+        help="The command",
+        choices=["terra", "upload", "parse_monitoring_log", "cromwell"],
+    )
+    parser.add_argument("command_args", help="The command arguments", nargs=argparse.REMAINDER)
+    parser.add_argument("-v", "--version", action="version", version=version("altocumulus"))
     my_args = parser.parse_args()
 
     cmd = str2module[my_args.command]
-    sys.argv[0] = f'alto {my_args.command}'
+    sys.argv[0] = f"alto {my_args.command}"
     cmd.main(my_args.command_args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
