6e8b6ef2f98e99d3be710db0a4bd5dc7a768c447
Joshua Gould <joshua-gould@users.noreply.github.com>
2022-08-23 20:29:33 -0400

    Fixed imports
    Former-commit-id: 5943000125f13aa2065d5ce5aeaf5c41c51aa2aa

--- alto/utils/__init__.py
+++ alto/utils/__init__.py
@@ -1,17 +1,6 @@
 import subprocess
 from typing import List
 
-from .dockstore_utils import get_dockstore_workflow, parse_dockstore_workflow  # noqa: F401
-from .firecloud_utils import (  # noqa
-    get_firecloud_workflow,
-    get_workspace_info,
-    parse_firecloud_workflow,
-    parse_workspace,
-    submit_a_job_to_terra,
-    update_workflow_config_in_workspace,
-)
-from .io_utils import read_wdl_inputs, upload_to_cloud_bucket  # noqa: F401
-
 
 prefix_float = "_&@&_"
 
@@ -31,3 +20,15 @@ def run_command(
 
     if not dry_run:
         subprocess.check_call(command, stdout=cur_stdout, stderr=cur_stderr)
+
+
+from .dockstore_utils import get_dockstore_workflow, parse_dockstore_workflow  # noqa: F401, E402
+from .firecloud_utils import (  # noqa: F401, E402
+    get_firecloud_workflow,
+    get_workspace_info,
+    parse_firecloud_workflow,
+    parse_workspace,
+    submit_a_job_to_terra,
+    update_workflow_config_in_workspace,
+)
+from .io_utils import read_wdl_inputs, upload_to_cloud_bucket  # noqa: F401, E402
